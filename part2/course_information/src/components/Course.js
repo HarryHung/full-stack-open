bd30f97e64b6c65dced359017c28edb025cffad4
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-02-28 23:47:17 +0000

    exercise 2.2-2.5

--- part2/course_information/src/components/Course.js
+++ part2/course_information/src/components/Course.js
@@ -1,12 +1,32 @@
-import Header from './Header'
-import Content from './Content'
-
 const Course = ({ course }) => {
     return (
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <Total parts={course.parts}/>
         </div>
+    )
+}
+
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => 
+                <Part key={part.id} part={part} />
+            )}
+        </div>
+    )
+}
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+    return (
+        <b>total of {total} exercises</b>
     )
 }
 

e441ae227ed7e3d599b1a9191008dfca3cc3e9ef
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-02 01:11:30 +0000

    exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Display = ({ person }) => {
+  return (
+    <div>{person.name}</div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    setPersons(
+      [...persons,
+      { name: newName}]
+    )
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map( person =>
+        <Display key={person.name} person={person}/>
+      )}
+    </div>
+  )
+}
+
+export default App

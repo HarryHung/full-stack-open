f91c02173b4359ccb1bc8d0bda09476a99fcbbf0
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-02 01:24:07 +0000

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,16 @@ import { useState } from 'react'
 
 const Display = ({ person }) => {
   return (
-    <div>{person.name}</div>
+    <div>{person.name} {person.number}</div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -19,23 +20,21 @@ const App = () => {
     } else {
       setPersons(
         [...persons,
-        { name: newName }]
+        { name: newName, number: newNumber }]
       )
     }
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange =(event) => setNewNumber(event.target.value)
   
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
-        <div>
-          name: <input onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleNameChange}/></div>
+        <div>number: <input onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       {persons.map( person =>

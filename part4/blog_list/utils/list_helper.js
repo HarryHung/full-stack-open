af98126ad556db9014835c328f88f7d0eacb309e
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-15 00:47:38 +0000

    exercise 4.5-4.6

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -6,7 +8,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return "empty array"
+  }
+  return blogs.reduce((pre, cur) => pre.likes < cur.likes ? cur : pre)
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return "empty array"
+  }
+
+  const countBy = _.countBy(blogs, 'author')
+  const countByReformat = _.map(countBy, (val, key) => ({ author: key, blogs: val }))
+  return _.maxBy(countByReformat, 'blogs')
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }

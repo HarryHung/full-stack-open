aaba7313ba1278bb0e07595dd5a5f78b94da25cb
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-04-12 01:19:49 +0100

    exercise 4.15

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blog')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app

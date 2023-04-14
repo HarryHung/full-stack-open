c914c260c1eb5a0ea79358d4cb98abafbc6889f2
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-04-15 00:48:43 +0100

    exercise 4.20

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -2,7 +2,6 @@ const logger = require('./logger')
 
 const tokenExtractor = (request, response, next) => {
     const authorization = request.get('authorization')
-    console.log(authorization)
     if (authorization && authorization.startsWith('Bearer ')) {
       request.token = authorization.replace('Bearer ', '')
     } else {

eb3b1e65b68255282017047bcd5de2d68bd74f2d
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-04-15 00:43:16 +0100

    exercise 4.20

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -1,5 +1,17 @@
 const logger = require('./logger')
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    console.log(authorization)
+    if (authorization && authorization.startsWith('Bearer ')) {
+      request.token = authorization.replace('Bearer ', '')
+    } else {
+      request.token = null
+    }
+
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -13,5 +25,6 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  tokenExtractor,
   errorHandler
 }

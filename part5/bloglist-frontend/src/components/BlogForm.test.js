7aac742fbe09b3683f5ae67c49852c17a8526bf4
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-05-15 18:01:08 +0100

    exercise 5.16

---
+++ part5/bloglist-frontend/src/components/BlogForm.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import BlogForm from './BlogForm'
+import userEvent from '@testing-library/user-event'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createBlog = jest.fn()
+  const user = userEvent.setup()
+
+  render(<BlogForm createBlog={createBlog} />)
+
+  const inputs = screen.getAllByRole('textbox')
+  const sendButton = screen.getByText('create')
+
+  await user.type(inputs[0], 'Test Title')
+  await user.type(inputs[1], 'Test Author')
+  await user.type(inputs[2], 'https://www.test.com')
+  await user.click(sendButton)
+
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].title).toBe('Test Title')
+  expect(createBlog.mock.calls[0][0].author).toBe('Test Author')
+  expect(createBlog.mock.calls[0][0].url).toBe('https://www.test.com')
+})

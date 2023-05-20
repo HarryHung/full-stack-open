2ccfeee078bc2db479b2da4aea6c48f12c490c24
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-05-20 22:48:11 +0100

    exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'test_user',
+      password: 'password123'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,25 @@ describe('Blog app', function() {
     cy.contains('Log in to application')
     cy.get('input').should('have.length', 2)
     cy.get('button').contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('test_user')
+      cy.get('#password').type('password123')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('test_user')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.notification')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })

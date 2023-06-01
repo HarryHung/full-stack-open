26d9d61bdc597ee85facfb5a367051cfa838df4e
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-06-02 00:25:16 +0100

    exercise 6.4

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -31,6 +31,8 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote 
       )
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
     default: 
       return state 
   }
@@ -40,6 +42,17 @@ export const addVote = (id) => {
   return {
     type: 'VOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
